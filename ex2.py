94f5bed112d41f604d22ac51fb55910161bc7ab0
htethtethtunjewel <htethtethtunjewel@gmail.com>
2018-06-05 09:18:09 +0630

    Ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#A commit,this is so you canread your program later.
+#Anything after the # is ignored by python.
+print("I could have code like this.")#said the comment after is ignored
+#You can also use a comment to "disable" or comment out code:
+#print("This worn't run")
+print ("This will run")
