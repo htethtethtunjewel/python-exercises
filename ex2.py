5b60f0f724589b2ad5ac517cb6bd46035bf60261
htethtethtunjewel <htethtethtunjewel@gmail.com>
2018-06-05 12:25:05 +0630

    Python Exercise-2

--- ex2.py
+++ ex2.py
@@ -3,4 +3,4 @@
 print("I could have code like this.")#said the comment after is ignored
 #You can also use a comment to "disable" or comment out code:
 #print("This worn't run")
-print ("This will run")
+print ("This will run.")
