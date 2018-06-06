3e8489e8e097c8c53a55dc1c576ef989c5c7d372
htethtethtunjewel <htethtethtunjewel@gmail.com>
2018-06-06 22:36:54 +0630

    Python Exercise-18

--- ex18.py
+++ ex18.py
@@ -1,14 +1,15 @@
+
 def print_two(*args):
-    arg1,arg2=args
+    arg1,arg2 =args
     print (f"arg1: {arg1},arg2: {arg2}")
 def print_two_again(arg1,arg2):
     print(f"arg1: {arg1},arg2: {arg2}")
-    def print_one(arg1):
+def print_one(arg1):
         print(f"arg1: {arg1}")
-        def print_none():
-            print("I got nothin'.")
-            print_two("Zed","Shaw")
-            print_two_again("Zed","Shaw")
-            print_one("First!")
-            print_none()
+def print_none():
+    print("I got nothin'.")
+print_two("Thu","Zaw")
+print_two_again("yes","no")
+print_one("First!")
+print_none()
 
