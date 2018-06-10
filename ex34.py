b4023f30576a910052232e7be16ee2fc7b6d91ec
htethtethtunjewel <htethtethtunjewel@gmail.com>
2018-06-10 19:14:25 +0630

    Python Exercise-34

---
+++ ex34.py
@@ -0,0 +1,16 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+python = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
+ 
+print(f"The animal at 1 is a {bear}")
+print(f"The third animal(3rd) is a {peacock} ")
+print(f"The fist(1st) animals is a {bear}")
+print(f"The animals at 3 is a {peacock}")
+print(f"The fifth(5th) animal is a {whale}")
+print(f"The animal at 2 is a {python}")
+print(f"The sixth animal is a {platypus}")
+print(f"The animal at 4 is a {kangaroo}")
