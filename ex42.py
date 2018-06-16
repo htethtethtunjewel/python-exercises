3a5455c395ae2c2c8c2e11ebb54c770e83740751
htethtethtunjewel <htethtethtunhtet@gmail.com>
2018-06-16 19:27:18 +0630

    Python Exercise-42

---
+++ ex42.py
@@ -0,0 +1,40 @@
+class Animal(object):
+   pass
+class Dog(Animal):
+    def __init__(self,name):
+        self.name=name
+class Cat(Animal):
+    def __init__(self,name):
+        self.name=name
+class Person(object):
+    def __init__(self,name):
+       self.name=name
+       self.pet=None
+class Employee(Person):
+    def __init__(self,name,salary):
+       super(Employee,self).__init__(name)
+       self.salary =salary
+class Fish(object):
+    pass
+class Salmon(Fish):
+    pass
+class Halibut(Fish):
+    pass
+rover=Dog("Rover")
+satan=Cat("Satan")
+mary=Person("Mary")
+mary.pet=satan
+frank=Employee("Frank",120000)
+flipper=Fish()
+course=Salmon()
+harry=Halibut()
+print(mary.pet)
+print(frank)
+print(frank.pet)
+print(flipper)
+print(course)
+print(harry)
+print(rover)
+print(satan)
+print(mary)
+
