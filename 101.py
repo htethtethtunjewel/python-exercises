d11bfc2de5576a6b0b62990728d5f5727e3c7b48
htethtethtunjewel <htethtethtunhtet@gmail.com>
2018-06-26 09:04:35 +0630

    101

---
+++ 101.py
@@ -0,0 +1,13 @@
+tabby_cat="\t I'm tabbled in."
+persian_cat="I'm split \n on a line,"
+backslash_cat="I'm \\a \\ cat."
+fat_car="""
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_car)
